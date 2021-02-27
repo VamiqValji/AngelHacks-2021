85de390551cfd0b74c0446dabc5ef0feace8ac0f
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2021-02-27 00:41:03 -0500

    video does playing and knows when stop and start

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import ReactPlayer from 'react-player/youtube'
+import { Console } from 'console'
+import React, { useState } from 'react'
+import ReactPlayer from 'react-player/lazy'
 
 interface RoomProps {
 
 }
 
 const Room: React.FC<RoomProps> = ({}) => {
-    var currentVideo = ""
-    var queue = ["https://www.youtube.com/watch?v=iv8rSLsi1xo&ab_channel=AnsonAlexander", "https://www.youtube.com/watch?v=e91M0XLX7Jw"];
+
+    
+
+    var newVideo = "https://www.youtube.com/watch?v=Rq5SEhs9lws"
+
+    const start = () => {
+        
+        isPlaying = true;
+        alert(isPlaying)
+    }
+    
+    const pause = () => {
+        isPlaying = false;
+        alert(isPlaying)
+    }
+
+    const [video, setVideo] = useState("https://youtu.be/e91M0XLX7Jw")
+
+    function nextVideo() {
+        setVideo(prevVideo => prevVideo = newVideo)
+    }
+
+    var isPlaying = false;
 
     return (
         <div>
-            <ReactPlayer url='https://www.youtube.com/watch?v=e91M0XLX7Jw' controls={true} volume={0.5} />
-
+            <ReactPlayer url={video} controls={true} volume={0.5} onPlay={start} onPause={pause} onEnded={nextVideo} playing = {isPlaying} />
+            
             
         </div>
     );
