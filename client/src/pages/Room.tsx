dc3840d8ac9be68ece26cbbb8197d40cce441603
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2021-02-27 04:43:04 -0500

    video done need style
    video is pretty sick, queue working skip button

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,49 +1,89 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import ReactPlayer from 'react-player/lazy'
+import CanvasDraw from "react-canvas-draw";
 
-interface Props {}
+interface RoomProps {}
 
 
 const Room: React.FC<RoomProps> = ({}) => {
+    const inputRef = useRef<any>(null);
 
     const [currentVideo, setCurrentVideo] = useState<string>("");
 
     const [queue, setQueue] = useState<string[]>([]);
 
     useEffect(() => {
-        setQueue((prev) => [...prev,"https://www.youtube.com/watch?v=iv8rSLsi1xo&ab_channel=AnsonAlexander"]);
-        setCurrentVideo("https://www.youtube.com/watch?v=iv8rSLsi1xo&ab_channel=AnsonAlexander");
+        //setQueue((prev) => [...prev,"https://www.youtube.com/watch?v=iv8rSLsi1xo&ab_channel=AnsonAlexander"]);
+        setCurrentVideo("https://www.youtube.com/watch?app=desktop&feature=share&v=ZbZSe6N_BXs");
     }, [setQueue, setCurrentVideo])
 
-    const changeUrl = (url:any) => {
-        setCurrentVideo(url);
-    }
+    function nextVideo() {
+        setCurrentVideo(queue[0])
+        let temps = queue;
+        temps.shift();
+        setQueue(temps);
 
-    var newVideo = "https://www.youtube.com/watch?v=Rq5SEhs9lws"
+    }
+    var isPlaying = false;
 
     const start = () => {
         
         isPlaying = true;
-        alert(isPlaying)
     }
     
     const pause = () => {
         isPlaying = false;
-        alert(isPlaying)
     }
 
-    const [video, setVideo] = useState("https://youtu.be/e91M0XLX7Jw")
 
-    function nextVideo() {
-        setVideo(prevVideo => prevVideo = newVideo)
+
+    const [data,setData] = useState(String);
+    function getData(val:any) {
+        
+        if(val != "") { 
+             setData(val)
+        }
+        
     }
 
-    var isPlaying = false;
+    function clicked() {
+        if(data != "") {
+        setQueue((prev) => [...prev, data]);
+        }
+    }
 
+    function ListItem(props:any) {
+        return (
+        <li style={{color: "red"}}>{props.value}</li>
+        )
+    }
+
+    function sharingIsCaring() {
+
+    }
+
+    
     return (
         <div>
-            <ReactPlayer url={video} controls={true} volume={0.5} onPlay={start} onPause={pause} onEnded={nextVideo} playing = {isPlaying} />
+            <h1>Current Video: <a href={currentVideo}> </a> </h1>
+            <ReactPlayer url={currentVideo} controls={true} volume={0.5} onPlay={start} onPause={pause} onEnded={nextVideo} playing = {isPlaying} style={{ margin: "0 auto"}}/>
+            <input type="text" size= {50} ref={inputRef} onChange={() => {getData(inputRef.current.value)}}  style={{   display: "block", margin:"auto"}}/>
+            <button  style={{   display: "block", margin:"auto"}} onClick={ (e)=> {
+                clicked()
+                inputRef.current.value = ""
+                setData("")} }>Enter The URL And Click Me!</button>
+            <button style={{display: "block", margin:"auto"}} onClick ={()=> {nextVideo()}}>click to skip</button>
+            <h2>Queue:</h2>
+            <ul>
+                {queue.map((number) =>
+                 <ListItem key={number.toString()}
+                value={number} />
+                )}
+            </ul>
+
+            <CanvasDraw getSaveData={sharingIsCaring} style={{ float:"right", position: "relative", top: "3px"}} canvasHeight = {700}/>
         </div>
+            
     )
 }
 
