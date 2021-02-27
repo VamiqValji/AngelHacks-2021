b3b26d6958d00578d0ca8163274147ae7b5ff076
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2021-02-26 23:01:46 -0500

    Revert "video good i think"
    This reverts commit e0058e425ecc13a244185e1aa1ffbe8d79b58b6b.

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
-import ReactPlayer from 'react-player/youtube'
-
 interface RoomProps {
 
 }
 
 const Room: React.FC<RoomProps> = ({}) => {
-    var currentVideo = ""
-    var queue = ["https://www.youtube.com/watch?v=iv8rSLsi1xo&ab_channel=AnsonAlexander", "https://www.youtube.com/watch?v=e91M0XLX7Jw"];
-
     return (
         <div>
-            <ReactPlayer url='https://www.youtube.com/watch?v=e91M0XLX7Jw' controls={true} volume={0.5} />
+            
         </div>
     );
 }
