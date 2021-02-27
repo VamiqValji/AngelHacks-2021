f1510d05c9b512721b757d41ae15f1b5f3e17690
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2021-02-26 22:32:37 -0500

    Update Room.tsx

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
-
 interface RoomProps {
 
 }
 
 const Room: React.FC<RoomProps> = ({}) => {
     return (
-        <>
-            <div className="containerContainer">
-                <div className="container">
-                    <h2>Room</h2>
-                </div>
-            </div>
-        </>
+        <div>
+            
+        </div>
     );
 }
 
