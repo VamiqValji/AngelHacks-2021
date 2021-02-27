f1f850825e1cbc94cba830e1847b74311bf2f4e0
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2021-02-26 23:12:35 -0500

    Update CreateRoom.tsx

--- client/src/pages/CreateRoom.tsx
+++ client/src/pages/CreateRoom.tsx
@@ -5,11 +5,22 @@ interface CreateRoomProps {
 }
 
 const CreateRoom: React.FC<CreateRoomProps> = ({}) => {
+<<<<<<< HEAD
+=======
+    const roomName = useRef<HTMLInputElement>(null);
+>>>>>>> parent of d00a724 (create room //  merge conflict fixed)
     return (
         <>
             <div className="containerContainer">
                 <div className="container">
+<<<<<<< HEAD
                     <h2>Create</h2>
+=======
+                    <h2>Create Room</h2>
+                    <input className="center" ref={roomName} type="text"/>
+                    <br/>
+                    <button className="center" >Create</button>
+>>>>>>> parent of d00a724 (create room //  merge conflict fixed)
                 </div>
             </div>
         </>
