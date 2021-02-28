48ea55a5d198ff2d22da88853e03f777b25fbe2c
OwlEpic <60725111+OwlEpic@users.noreply.github.com>
2021-02-28 11:16:21 -0500

    home pg

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { watch } from 'fs';
 import React from 'react'
+import img  from '../imgs/logo.svg'
 
 interface HomeProps {
 
@@ -9,7 +11,15 @@ const Home: React.FC<HomeProps> = ({}) => {
         <>
             <div className="containerContainer">
                 <div className="container">
-                    <h2>Home</h2>
+                    <h1 className="center"><span className="watch">watch</span>Socket</h1>
+                    <img src={img} alt="logo"  className="center"
+                    style = { {margin: "auto", width: "500px", height: "482px"}}
+                    />
+                    <br/>
+                    <br/>
+                    <h3>watchSocket is a website you can use to watch all your favourite video's with your friends!</h3>
+                    <h3 className="center">Click Create To Start!</h3>
+
                 </div>
             </div>
         </>
