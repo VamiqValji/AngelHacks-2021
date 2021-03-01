a37123bcbbf816412f0da951ccb56f81b79d544a
Vamiq <vamiq.valji@gmail.com>
2021-02-28 20:28:15 -0500

    fix endpoint

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -17,7 +17,7 @@ const Room: React.FC<RoomProps> = ({}) => {
     
     const inputRef = useRef<any>(null);
 
-    const ENDPOINT = "https://watchsocket.herokuapp.com/socket";
+    const ENDPOINT = "/";
 
     let roomID:any = useParams();
     console.log(roomID);
