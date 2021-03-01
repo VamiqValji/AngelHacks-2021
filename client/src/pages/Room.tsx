24ceee83177de084c6df5feaf2ae3db3222e31de
Vamiq <vamiq.valji@gmail.com>
2021-02-28 20:06:53 -0500

    fix

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -17,7 +17,7 @@ const Room: React.FC<RoomProps> = ({}) => {
     
     const inputRef = useRef<any>(null);
 
-    const ENDPOINT = "http://localhost:3001";
+    const ENDPOINT = "https://watchsocket.herokuapp.com";
 
     let roomID:any = useParams();
     console.log(roomID);
